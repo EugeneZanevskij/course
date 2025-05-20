3563ace3adda3314da93713e6c6f20e1367ea3be
Eugene <zanevskijzenia@gmail.com>
2025-05-20 22:13:09 +0300

    верстка, изменение классов, добавление элементов

--- watch.js
+++ watch.js
@@ -1,9 +1,34 @@
-console.log(1);
+// console.log(1);
+
+// const button = document.querySelector('.hero__content-button');
+// console.log(button);
+
+// button.addEventListener("click", () => {
+//     console.log('click');
+//     button.style.display = "none";
+// });
 
 const button = document.querySelector('.hero__content-button');
-console.log(button);
 
-button.addEventListener("click", () => {
-    console.log('click');
-    button.style.display = "none";
+button.addEventListener('click', () => {
+    button.classList.toggle("hero__content-button--secondary");
+    addListItem(prompt("Введи текст"));
 });
+
+// const element = document.createElement('p');
+// element.textContent = "Hiiiiiiiii";
+// element.className = "header-3";
+// console.log(element);
+
+// document.body.appendChild(element);
+
+// const list = document.querySelector('.abilities__list');
+// list.appendChild(element);
+const list = document.querySelector('.abilities__list');
+
+function addListItem(text) {
+    const element = document.createElement('p');
+    element.textContent = text;
+    element.className = "header-3";
+    list.appendChild(element);
+}
