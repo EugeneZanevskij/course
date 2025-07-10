3cc50fdbd35d858942ac1ac8d56aead516327ba0
Eugene <zanevskijzenia@gmail.com>
2025-07-10 22:40:00 +0300

    Деструктуризация - задание

--- destr.js
+++ destr.js
@@ -77,3 +77,19 @@ value2 = value1 //10
 value1 = c //15
 
 [value1, value2] = [value2, value1] //[15, 10]
+
+// Напишите деструктурирующее присваивание, которое:
+
+// свойство name присвоит в переменную name.
+// свойство years присвоит в переменную age.
+// свойство isAdmin присвоит в переменную isAdmin (false, если нет такого свойства)
+let user = {
+  name: "John",
+  years: 30
+};
+
+let { name, years: age, isAdmin = false } = user
+
+alert( name ); // John
+alert( age ); // 30
+alert( isAdmin ); // false
